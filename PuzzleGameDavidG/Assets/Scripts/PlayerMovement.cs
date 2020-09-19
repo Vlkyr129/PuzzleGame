78caa5a44c6b5e58a4ef8b9951198251290c599e
Vlkyr129 <defcon129@gmail.com>
2020-09-19 19:58:20 -0400

    Characrter movement fix and pick up item fix

---
+++ PuzzleGameDavidG/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
--- PuzzleGameDavidG/Assets/Scripts/pickUpObject.cs
+++ PuzzleGameDavidG/Assets/Scripts/pickUpObject.cs
@@ -7,41 +7,39 @@ public class pickUpObject : MonoBehaviour
 {
     public Transform player;
     public Transform playerCamera;
-    public float throwForce = 10;
+    [SerializeField]
+    float throwForce = 100;
     [SerializeField]
     float pickUpDistance = 3;
     float distanceToObject;
-    bool hasPlayer;
     bool isBeingCarried = false;
     bool collideWorld = false;
+    float timePassed;
 
     private void Update()
     {
         distanceToObject = Vector3.Distance(gameObject.transform.position, player.position);
-        
-        if (distanceToObject <= pickUpDistance && Input.GetButtonDown("E"))
+
+        timePassed++;
+
+        if (distanceToObject <= pickUpDistance && Input.GetMouseButtonDown(0) && isBeingCarried == false)
         {
             GetComponent<Rigidbody>().isKinematic = true;
             transform.parent = playerCamera;
             isBeingCarried = true;
+            timePassed = 0;
         }
-        if (isBeingCarried)
+        if (isBeingCarried && timePassed >= 1)
         {
-            //Drops item if collides with worl objects
-            if (collideWorld)
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                isBeingCarried = false;
-                collideWorld = false;
-            }
+            
             //Allows player to throw carried item
             if (Input.GetMouseButtonDown(0))
             {
                 GetComponent<Rigidbody>().isKinematic = false;
                 transform.parent = null;
-                isBeingCarried = false;
+                
                 GetComponent<Rigidbody>().AddForce(playerCamera.forward * throwForce);
+                isBeingCarried = false;
             }
             //Allows player to simply drom item without any force
             if (Input.GetMouseButtonDown(1))
@@ -50,12 +48,26 @@ public class pickUpObject : MonoBehaviour
                 transform.parent = null;
                 isBeingCarried = false;
             }
+            //Drops item if collides with worl objects
+            if (collideWorld)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                isBeingCarried = false;
+                collideWorld = false;
+            }
+        }
+
+        //Reset gameObject
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = new Vector3(0, 10, 0);
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isBeingCarried)
+        if (isBeingCarried && other.tag != "Ground")
         {
             collideWorld = true;
         }
