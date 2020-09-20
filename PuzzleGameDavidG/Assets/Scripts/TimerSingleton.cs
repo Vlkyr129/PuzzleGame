c6a9337e3b6b8b8d99f7c8a174a9fc96eaa3a975
Vlkyr129 <defcon129@gmail.com>
2020-09-20 17:40:05 -0400

    SingleTon being added

---
+++ PuzzleGameDavidG/Assets/Scripts/TimerSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerSingleton : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public int timer;
+
+    public static TimerSingleton instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);        
+        }
+    }
+
+    public void addTimer()
+    {
+        timer++;
+    }
+}
