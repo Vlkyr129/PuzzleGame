f2ab36920175f1f4d3115dbb3c6d810fc03b026d
Vlkyr129 <defcon129@gmail.com>
2020-09-19 21:18:23 -0400

    Fixing EndGame

--- PuzzleGameDavidG/Assets/Scripts/EscapeDoor.cs
+++ PuzzleGameDavidG/Assets/Scripts/EscapeDoor.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EscapeDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("TheEnd");
+        }
     }
 }
