43afaaf9108d6d48b685751c740238f5601866b8
Vlkyr129 <defcon129@gmail.com>
2020-09-20 20:30:18 -0400

    Acutally added singleton?

--- PuzzleGameDavidG/Assets/Scripts/Score.cs
+++ PuzzleGameDavidG/Assets/Scripts/Score.cs
@@ -3,24 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Score : MonoBehaviour
+public class Score : Singleton<Score>
 {
     public TextMeshProUGUI scoreText;
     private int score;
-
-    public static Score instance;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 
     public void AddScore()
     {
